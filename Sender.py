37b2cdf990fbe88a8ca1557673218f2aa81579ef
Jim <jimfilippou@hotmail.gr>
2016-12-09 15:09:19 +0200

    Minor Fixes

--- Sender.py
+++ Sender.py
@@ -1,7 +1,8 @@
 from fabric.api import local
-import os,time
+import os, time
 
 run = True
+
 
 def file_len(fname):
     with open(fname) as f:
@@ -9,29 +10,32 @@ def file_len(fname):
             pass
     return i + 1
 
-def GetWindowID(current=False):
+
+def GetWindowID(current=False, name="Mozilla Firefox"):
     if current:
         return int(local("xdotool getactivewindow", True))
     else:
-        return int(local("xdotool search --name \"Mozilla Firefox\"", True))
+        return int(local("xdotool search --name \"%s\"" %name, True))
+
 
 def GetSent(index):
     with open('sentences.txt') as f:
         sequence = f.read().splitlines()
         return sequence[index]
 
+
 def main():
-    while run:
-        for s in range(file_len("sentences.txt")):
-            id = GetWindowID()
-            sent = GetSent(s)
-            # Needs To Be Minimized
-            os.system("xdotool windowfocus --sync %s" %(id))
-            os.system("xdotool type %s" %(sent))
-            os.system("xdotool key KP_Enter")
-            time.sleep(5)
+    for s in range(file_len("sentences.txt")):
+        identity = GetWindowID()
+        sent = GetSent(s)
+        # Needs To Be Minimized
+        os.system("xdotool windowfocus --sync %s" % identity)
+        os.system("xdotool type --delay 250 \"%s\"" % sent)
+        os.system("xdotool key KP_Enter")
+        time.sleep(5)
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
+    print("Wating 10 seconds...")
+    time.sleep(10)
     main()
